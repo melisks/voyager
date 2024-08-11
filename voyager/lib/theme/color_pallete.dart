3355c0ae6afb0bcba7e49b8a57c2c9c200d37c63
melisks <meliodassks2001@gmail.com>
2024-08-11 12:13:03 +0530

    color

--- voyager/lib/theme/color_pallete.dart
+++ voyager/lib/theme/color_pallete.dart
@@ -4,6 +4,7 @@ class AppColor {
   static Color ckbackground = Color(0xFF17181c);
   static Color ckdarkgrey = Color(0xFF);
   static Color ckgrey = Color(0xFF3a393d);
+  static Color ckwhite = Color(0xFFffffff);
   static Color ckcontainer = Color(0xFF2a2a2e);
   static Color ck1 = Color(0xFF202427);
   static Color ck2 = Color(0xFF363b40);
