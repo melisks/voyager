4894c64cf3c7c58a075f10fe8aea0f8f53d533ef
melisks <meliodassks2001@gmail.com>
2024-08-11 12:04:22 +0530

    bottom nav

---
+++ voyager/lib/widgets/mybottomnav.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:voyager/theme/color_pallete.dart';
+
+class MyBottomNavigationBar extends StatefulWidget {
+  const MyBottomNavigationBar({super.key});
+
+  @override
+  State<MyBottomNavigationBar> createState() => _MyBottomNavigationBarState();
+}
+
+class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      decoration: BoxDecoration(
+          color: AppColor.ckgrey.withOpacity(0.5),
+          borderRadius: BorderRadius.circular(50)),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
