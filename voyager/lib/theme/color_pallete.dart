27cd5706d52d4cf5c04bdbd2d3c489498ce41785
melisks <meliodassks2001@gmail.com>
2024-08-11 12:11:24 +0530

    color change

--- voyager/lib/theme/color_pallete.dart
+++ voyager/lib/theme/color_pallete.dart
@@ -3,10 +3,9 @@ import 'package:flutter/material.dart';
 class AppColor {
   static Color ckbackground = Color(0xFF17181c);
   static Color ckdarkgrey = Color(0xFF);
-  static Color ckwhite = Color(0xFFffffff);
   static Color ckgrey = Color(0xFF3a393d);
   static Color ckcontainer = Color(0xFF2a2a2e);
-  static Color ck1 = Color(0xFF000000);
+  static Color ck1 = Color(0xFF202427);
   static Color ck2 = Color(0xFF363b40);
   static Color ck3 = Color(0xFF44494f);
 }
